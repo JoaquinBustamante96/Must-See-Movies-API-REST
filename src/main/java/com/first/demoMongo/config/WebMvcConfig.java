1445178f749f31336224f90090df7b05ee5880d6
Joaquin <joako.95_@hotmail.com>
2019-04-24 03:21:57 -0300

    added getOrigins in webMvcConfig

--- src/main/java/com/first/demoMongo/config/WebMvcConfig.java
+++ src/main/java/com/first/demoMongo/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.first.demoMongo.config;
 
 import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,16 +13,25 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @EnableWebMvc
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${allowed.origins}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
-                .allowedOrigins("*")
+                .allowedOrigins(getOrigins())
                 .allowedHeaders("*").maxAge(3600);
+    }
+
+    private String[] getOrigins(){
+        return allowedOrigins.split(",");
     }
 
 }
